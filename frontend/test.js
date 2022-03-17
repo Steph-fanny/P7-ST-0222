195773d282c7cb14a0c5ebc6bc0b2cd930048a29
steph-fanny <stephdvweb@gmail.com>
2022-03-17 09:34:49 +0100

    auth vue3

--- frontend/test.js
+++ frontend/test.js
@@ -1,42 +1,7 @@
 
-/*{ 
 
 
-
-
-
-<!-- <p class="login-tittle">Votre compte</p> 
-
-    <div class=" mx-auto" style="width: 350px">
-      <form class="form-login ">
-        <div class="form-group ">
-          <label for="email">Email: </label>
-          <input v-model= "dataLogin.email"
-          type="email" 
-          class="form-control" 
-          id="email" 
-          placeholder="email@example.com" required>
-        </div>
-
-        <div class="form-group ">
-          <label for="Password">Password</label>
-          <input 
-          v-model= "dataLogin.password"
-          type="password" 
-          class="form-control" 
-          id="Password" 
-          placeholder="Password" required>
-      </div>
-      
-      <button 
-      @click.prevent="loginAccount"
-      type="submit" 
-      class="btn btn-primary" 
-      style="width: 250px">Se connecter</button>
-      </form>
-      <div class="inscription"></div>
-        <nav class="navlogsign">Vous n'avez pas de compte ?<br>
-        <router-link to = "/signup">Rejoignez-nous !</router-link></nav>  */
+import affichagePostVue from "@/components/affichagePost.vue"
 
 
 
@@ -109,29 +74,112 @@
 
 
 
-post 
-<div class ="card-post">  
-    <div class="card-body col d-flex justify-content-center">        
-        <div class="card-body">           
+
+
+
+commentaire
+<div class="container mt-5">
+        <div class="commentaire d-flex justify-content-center row">
+        <div class=" col-md-8">            
+                </div>
+                <div class="bg-light p-2">
+                    <div class="d-flex flex-row align-items-start">
+                        <img class="rounded-circle" src="https://i.imgur.com/RpzrMR2.jpg" width="40">
+                        <textarea class="form-control ml-1 shadow-none textarea">
+                            
+                            </textarea></div>
+                    <div class="mt-2 text-right">
+                        <button class="send btn btn-primary btn-sm shadow-none " 
+                            type="button"
+                            @click.prevent= "postComment()"
+                            >Post comment</button>
+
+                    <button class=" send btn btn-primary btn-sm shadow-none ml-1 " type="button">Cancel</button></div>
+                </div>
+            </div>
+        </div> 
+
+
+
+        affichagePostVue
+
+        <div>
+    
+    <div class="container-fluid gedf-wrapper">
+        <div class="row">
+                        
+            <div class="col-md-6 gedf-main">
                 
-                    <form id="form-signup">
-                        <div class="form-group">                                   
-                            <label class="sr-only" for="message">Titre du message :</label>
-                            <input type="text" id="title" name="title" class="form-control" 
-                            required v-model="inputMessage.title"/>
+                <!--- \\\\\\\Post-->
+                <div class="card gedf-card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="mr-2">
+                                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
+                                </div>
+                                <div class="ml-2">
+                                    <div class="h5 m-0">@LeeCross</div>
+                                    <div class="h7 text-muted">Miracles Lee Cross</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="dropdown">
+                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                        <div class="h6 dropdown-header">Configuration</div>
+                                        <a class="dropdown-item" href="#">Save</a>
+                                        <a class="dropdown-item" href="#">Hide</a>
+                                        <a class="dropdown-item" href="#">Report</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">                                   
-                            <label class="sr-only" for="content">contenu:</label>
-                            <textarea type="text" id="content" 
-                            name="content" rows="3" class="form-control" 
-                            placeholder="Que voulez-vous partager aujourd'hui?"
-                            required v-model="inputMessage.content">                                
-                            </textarea> 
-                        </div>  
-                    </form>                               
-                    <button class="btn btn-primary" v-on:click="sendMessage">Publier</button>
-                                                            
-                                                          
+
+                    </div>
+                    <div class="card-body">
+                        <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
+                        <a class="card-link" href="#">
+                            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
+                        </a>
+
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
+                            sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
+                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
+                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
+                    </div>
+                </div>                
+                                           
+            </div>
         </div>
     </div>
-</div>
+
+
+
+
+    profil modifier
+    / let url = `http://localhost:8080/api/user/${ this.userAccount.formData }`;
+//     let options = {
+//       method: "PUT",
+//       headers: {
+//         'Authorization': 'Bearer ' + localStorage.getItem("token"),
+//       }
+//     };
+//     fetch(url, options)
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data)
+//         this.userAccount = data.userAccount
+//         this.image = data.image;       
+//       })
+//       .catch(error => console.log(error))
+//     },
+
+
